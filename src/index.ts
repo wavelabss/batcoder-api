b56ea8a564d9a3e9a8bf7451659ebbafc58396f2
brunolaviso <bruno.laviso@hotmail.com>
2021-11-18 15:45:54 -0300

    chore: add cors to the project

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import routes from './routes/challenges'
 import { connect } from './database'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -9,4 +10,5 @@ const app = express()
 connect()
 app.listen(process.env.PORT)
 app.use(express.json())
+app.use(cors())
 app.use(routes)
